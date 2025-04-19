b80e8e96619be263a0cdd6d7b3160e7040460274
tylerd92 <twdeans@gmail.com>
2025-04-19 00:45:44 -0400

    create models and started on auth code

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  console.log("Signup route hit");
+};
+
+export const login = (req, res) => {
+  console.log("Login route hit");
+};
+
+export const logout = (req, res) => {
+  console.log("Logout route hit");
+};
---
+++ backend/models/todo.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+export default Todo;
