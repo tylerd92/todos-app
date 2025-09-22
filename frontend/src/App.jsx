51d13bc50909e07bc023eb06b9218328033d1718
tylerd92 <twdeans@gmail.com>
2025-09-21 23:43:11 -0400

    set up react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
+import { Navigate, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      <SignUp />
+      <Routes>
+        <Route
+          path='/'
+          element={authUser ? <Home /> : <Navigate to='/login' />}
+        />
+        <Route
+          path='/login'
+          element={authUser ? <Navigate to='/' /> : <Login />}
+        />
+        <Route
+          path='/signup'
+          element={authUser ? <Navigate to='/' /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
     </>
   );
 }
