d26b2b4619a6e564aa71bb1b2d3dc00fdc879d11
tylerd92 <twdeans@gmail.com>
2025-10-03 20:29:35 -0400

    set up for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import "dotenv/config";
 
 import todoRoutes from "./routes/todo.routes.js";
@@ -9,13 +10,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hey now brown cow!");
-});
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.listen(PORT, () => {
   connectToMongoDB();
